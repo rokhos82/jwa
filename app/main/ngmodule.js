b05413a3c0dfe5074d3661732184fd58fecd51ff
Justin Lane <justin.l.lane@gmail.com>
2020-11-10 10:13:10 -0700

    Bootstrap Migration 1
    Moved the welcome screen, the main component, and parts of the incident search component.

--- app/main/ngmodule.js
+++ app/main/ngmodule.js
@@ -8,4 +8,7 @@ import angular from "angular";
 import uiRouter from "@uirouter/angularjs";
 import {StickyStatesPlugin} from "@uirouter/sticky-states";
 import ngResource from 'angular-resource';
+import uiBootstrap from "angular-ui-bootstrap";
+import ngTouch from "angular-touch";
+import ngAnimate from "angular-animate";
 //import ocLazyLoad from 'oclazyload';
